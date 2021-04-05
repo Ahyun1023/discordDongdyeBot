e633a3d4a8b6d8e2d0d565f7a100f9e267601ea2
남아현 <43693054+Ahyun1023@users.noreply.github.com>
2021-04-05 20:36:09 +0900

    토큰 숨김

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const token = 'ODI4MjExNzczMTQ3ODQwNTIy.YGmSUg.-CfRAbHtMKDdcmLO984JgdJG8Ww';
+const token = '토큰은 너굴맨이 잘 처리해뒀다구~';
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`); 
