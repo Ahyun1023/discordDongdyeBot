068c78a39c53accc35c9621170988300962554cf
남아현 <ska6371@gmail.com>
2021-04-06 20:30:25 +0900

    이미지, 파일 등을 전송하기

--- index.js
+++ index.js
@@ -12,8 +12,23 @@ client.on('message', msg => {
          msg.reply('Pong!'); 
     } 
 
-    if(msg.content == '!myProfile'){
+    if(msg.content === '!myProfile'){
         msg.reply(msg.author.displayAvatarURL());
+    }
+
+    if(msg.content === '!rip'){
+        var attachment = new Discord.MessageAttachment("https://i.imgur.com/w3duR07.png");
+        msg.channel.send(`${msg.author},`, attachment);
+    }
+
+    if(msg.content === '!swordCow'){
+        var attachment = new Discord.MessageAttachment("./file/image/검문소.png");
+        msg.channel.send(`${msg.author}, 당신을 지켜줄...`, attachment);
+    }
+
+    if(msg.content === '!easyLife'){
+        var attachment = new Discord.MessageAttachment("./file/txt/쉽게사는 방법.txt")
+        msg.channel.send(`${msg.author}, 당신에게 필요한`, attachment);;
     }
 });
 
