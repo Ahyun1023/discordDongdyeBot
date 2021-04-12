51aac9267f4b353481b4e7f6ce46c5d4f0421027
남아현 <ska6371@gmail.com>
2021-04-12 20:46:36 +0900

    메세지 보내는 부분 모듈화

---
+++ message.js
@@ -0,0 +1,61 @@
+const Discord = require('discord.js');
+
+function asdf(msg) {
+
+    if (msg.content === 'ping') {
+        msg.reply('Pong!');
+    }
+
+    // 프로필
+    if (msg.content === '!myProfile') {
+        msg.reply(msg.author.displayAvatarURL());
+    }
+
+    // 외부 사이트 이미지 첨부
+    if (msg.content === '!rip') {
+        var attachment = new Discord.MessageAttachment("https://i.imgur.com/w3duR07.png");
+        msg.channel.send(`${msg.author},`, attachment);
+    }
+
+    // 이미지 첨부
+    if (msg.content === '!swordCow') {
+        var attachment = new Discord.MessageAttachment("./file/image/검문소.png");
+        msg.channel.send(`${msg.author}, 당신을 지켜줄...`, attachment);
+    }
+
+    // 파일 첨부
+    if (msg.content === '!easyLife') {
+        var attachment = new Discord.MessageAttachment("./file/txt/쉽게사는 방법.txt")
+        msg.channel.send(`${msg.author}, 당신에게 필요한`, attachment);;
+    }
+
+    // 임베드 설명
+    if (msg.content === '임베드') {
+        // 임베드 생성
+        const embed = new Discord.MessageEmbed();
+        embed.setTitle(0xff0000).setDescription('안녕하세요! 임베드 설명입니다.');
+        embed.setDescription('설명!');
+
+        // 링크 이동
+        embed.setAuthor('Author', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org');
+
+        // 썸네일 추가
+        embed.setThumbnail('https://i.imgur.com/wSTFkRM.png');
+
+        // 필드 추가
+        embed.addField('Regular field title', 'Some value here');
+
+        // 시간 출력
+        embed.setTimestamp();
+
+        // 이미지 추가
+        embed.setImage('https://i.imgur.com/wSTFkRMs.png');
+
+        // 하단
+        embed.setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
+
+        msg.channel.send(embed);
+    }
+}
+
+module.exports.asdf = asdf;
