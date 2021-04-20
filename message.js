6ad4ec6066ccabf4aeb5c4fbb712504608101e13
남아현 <ska6371@gmail.com>
2021-04-20 20:41:12 +0900

    임베드 수정

--- message.js
+++ message.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
 
 function commandEvent(msg) {
-    console.log('commandEvent');
     // 프로필
     if (msg.content === '!myProfile') {
         msg.reply(msg.author.displayAvatarURL());
@@ -9,7 +8,6 @@ function commandEvent(msg) {
 }
 
 function msgFileEvent(msg){
-    console.log('msgFileEvent');
     // 외부 사이트 이미지 첨부
     if (msg.content === '!rip') {
         var attachment = new Discord.MessageAttachment("https://i.imgur.com/w3duR07.png");
@@ -24,17 +22,16 @@ function msgFileEvent(msg){
 }
 
 function embedEvent(msg){
-    console.log('embedEvent');
     // 임베드 생성
     const embed = new Discord.MessageEmbed();
     embed.setTitle(0xff0000).setDescription('안녕하세요! 임베드 설명입니다.');
     embed.setDescription('설명!');
 
     // 링크 이동
-    embed.setAuthor('Author', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org');
+    embed.setAuthor('안녕하세요! 도움말입니다!', 'https://i.imgur.com/wSTFkRM.png', 'https://github.com/Ahyun1023/discordDongdyeBot');
 
     // 썸네일 추가
-    embed.setThumbnail('https://i.imgur.com/wSTFkRM.png');
+    //embed.setThumbnail('https://i.imgur.com/wSTFkRM.png');
 
     // 필드 추가
     embed.addField('Regular field title', 'Some value here');
